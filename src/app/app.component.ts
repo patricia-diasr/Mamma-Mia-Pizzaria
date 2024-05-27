0ee9ef4258e8a96d4722cf988673cc6b56e3f08e
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-05-27 11:50:29 -0300

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Mamma-Mia-Pizzaria';
