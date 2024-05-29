c4504c6cc3417b53650fd10978fb2a5f68e5f125
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-05-29 12:20:40 -0300

    Slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  items = ['item 1', 'item 2', 'item 3', 'item 4', 'item 5'];
+  currentIndex = 0;
+  visibleItemsCount!: number;
+
+  constructor() {
+    this.updateVisibleItemsCount(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateVisibleItemsCount(event.target.innerWidth);
+  }
+
+  private updateVisibleItemsCount(windowWidth: number) {
+    if (windowWidth < 768) {
+      this.visibleItemsCount = 1;
+    } else if (windowWidth < 992) {
+      this.visibleItemsCount = 2;
+    } else {
+      this.visibleItemsCount = 3;
+    }
+  }
+
+
+  nextSlide() {
+    if (this.currentIndex < this.items.length) {
+      this.currentIndex++;
+    } else {
+      this.currentIndex = 0;
+    }
+  }
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    } else {
+      this.currentIndex = this.items.length;
+    }
+  }
+
+  visibleItems(){
+    const itemsToShow = [];
+    const currentIndex = this.currentIndex;
+    for (let i = 0; i < this.visibleItemsCount; i++) {
+        const index = (currentIndex + i) % this.items.length;
+        itemsToShow.push(this.items[index]);
+    }
+    return itemsToShow;
+  }
 
 }
