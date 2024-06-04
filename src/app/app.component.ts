a2df8656a06c606262bf6331faf82778d5af0215
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-06-04 11:01:38 -0300

    Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
     CommonModule, 
     RouterOutlet,
     HeaderComponent,
+    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
