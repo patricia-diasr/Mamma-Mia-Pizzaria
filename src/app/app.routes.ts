a4cc68efd032f94f0b26078e137e2ec793afcda3
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-06-06 20:47:38 -0300

    Reservation form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent },
     { path: 'cardapio', component: HomeComponent },
-    { path: 'reserva', component: HomeComponent },
+    { path: 'reserva', component: ReservationComponent },
     { path: 'fila', component: HomeComponent },
     { path: '**', redirectTo: '' }
 ];
