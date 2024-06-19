781f478698e06f83afa768845ea6d925caf63319
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-06-19 18:06:10 -0300

    Footer links and copy to clipboard

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { ToastComponent } from '../toast/toast.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+    selector: 'app-footer',
+    standalone: true,
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss',
+    imports: [
+      CommonModule,
+      ToastComponent
+    ]
 })
 export class FooterComponent {
+  showToast: boolean = false;
 
+  copyText(text: string): void {
+    navigator.clipboard.writeText(text)
+        .then(() => {
+          this.showToast = true;
+          setTimeout(() => this.showToast = false, 2500);
+        })
+        .catch(err => console.error('Error copying text: ', err));
+  }
 }
---
+++ src/app/components/toast/toast.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-toast',
+  standalone: true,
+  imports: [],
+  templateUrl: './toast.component.html',
+  styleUrl: './toast.component.scss'
+})
+export class ToastComponent {
+  @Input() toastText!: string;
+
+
+}
